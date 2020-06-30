1bf931563619437a0972946053d976434c935ad5
jennykathambi90 <jennykathambi90@gmail.com>
2020-06-30 21:15:32 +0300

    Added create new/edit blog post

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
+# Finally, we can say which field(s) should end up in our form. In this scenario we want only 
+# title and text to be exposed – author should be the person who is currently logged in (you!) 
+# and created_date should be automatically set when we create a post (i.e. in the code), right?
+
+# And that's it! All we need to do now is use the form in a view and display it in a template
