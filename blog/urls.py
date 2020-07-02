800498ad04bddb1555f35ecd57552733e205d4e2
jennykathambi90 <jennykathambi90@gmail.com>
2020-07-02 23:02:50 +0300

    Added draft page for unpublished views, publish button, delete button, security on views using decorators

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/edit/<int:pk>/', views.post_edit, name='post_edit'),
+    path('post/draft/', views.post_draft_list, name= 'post_draft_list'),
+    path('post/publish/<int:pk>/', views.post_publish, name= 'post_publish'),
+    path('post/remove/<int:pk>/', views.post_remove, name='post_remove')
 ]
